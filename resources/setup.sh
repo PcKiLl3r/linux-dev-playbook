e4312ac7828468b2c7bf59d699aaf1737c69e7ff
Grega Zorko <zorko.grega@gmail.com>
2024-09-27 09:04:21 +0200

    adjusted setup.sh

--- resources/setup.sh
+++ resources/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo dnf install ansible git
-git clone https://github.com/PcKiLl3r/linux-dev-playbook
-cd linux-dev-playbook
+git clone https://github.com/PcKiLl3r/linux-dev-playbook ~/personal/linux-dev-playbook
+cd ~/personal/linux-dev-playbook
 ansible-playbook main.yml
