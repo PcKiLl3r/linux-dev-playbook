75c6c38e1817d04dc2d4b5d925e8f91832ffb914
Grega Zorko <zorko.grega@gmail.com>
2024-09-27 09:21:19 +0200

    updating plays test on real machine

--- resources/setup.sh
+++ resources/setup.sh
@@ -3,4 +3,4 @@
 sudo dnf install ansible git
 git clone https://github.com/PcKiLl3r/linux-dev-playbook ~/personal/linux-dev-playbook
 cd ~/personal/linux-dev-playbook
-ansible-playbook main.yml
+ansible-playbook main.yml --vault-password-file ./vault/vault_pass.txt
